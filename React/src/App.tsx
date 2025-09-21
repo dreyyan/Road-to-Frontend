224588a5b14b4518b819cdba3c16192b95346022
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-09-21 12:24:43 +0800

    Worked on intermediate TypeScript and React problems, last worked on 08-EventHandlerTyping.tsx

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./problems/01-TypedButtonComponent";
 import Counter from "./problems/02-CounterComponent";
 import InputField from "./problems/03-InputFormField";
+import TodoItem from "./problems/04-ToDoItemComponent";
+
+import type { TodoProps } from "./problems/04-ToDoItemComponent";
+import List from "./problems/05-ListRenderer";
+import StatusMessage from "./problems/06-ConditionalRendering";
+import ProfileCard from "./problems/07-OptionalProps";
 
 const App = () => {
+  const [todo, setTodo] = useState<TodoProps>({
+    id: 100,
+    text: "Wash the dishes",
+    completed: false,
+  });
+
+  const toggleCheck = (id: number) => {
+    setTodo((prev: TodoProps) => ({ ...prev, completed: !prev.completed }));
+  };
+
   return (
     <div className="flex justify-center items-center">
       {/* Problem #1: Typed Button Component */}
@@ -12,8 +28,24 @@ const App = () => {
       {/* Problem #2: Counter Component */}
       {/* <Counter/> */}
 
-      {/* Problem #3: Input  Form Field */}
+      {/* Problem #3: Input Form Field */}
       {/* <InputField value="" onChange={(event) => console.log(event)}/> */}
+
+      {/* Problem #4: Todo Item Component */}
+      {/* <TodoItem todo={todo} onToggle={toggleCheck}/> */}
+
+      {/* Problem #5: List Renderer */}
+      {/* <List items={["A", "B", "C"]}/> */}
+
+      {/* Problem #6: Conditional Rendering */}
+      {/* <StatusMessage status="loading"/>
+      <StatusMessage status="error"/>
+      <StatusMessage status="success"/> */}
+
+      {/* Problem #7: Optional Props */}
+      <ProfileCard name="Juan" avatarUrl="juan-avatar.png"/>
+      <ProfileCard name="Chris"/>
+
     </div>
   );
 };
